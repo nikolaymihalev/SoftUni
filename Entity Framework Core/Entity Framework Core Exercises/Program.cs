83bbf743575d9cd68534a34e983d25c92ad7830c
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-10-27 08:05:46 +0000

    03. Employee Full Information

--- Entity Framework Core/Entity Framework Core Exercises/Program.cs
+++ Entity Framework Core/Entity Framework Core Exercises/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Entity_Framework_Core_Exercises
+﻿using Entity_Framework_Core_Exercises.Data.Models;
+
+namespace Entity_Framework_Core_Exercises
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SoftUniContext context = new SoftUniContext();
+            
+            //03. Employee Full Information
+            Console.WriteLine(GetEmployeesFullInformation(context));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees
+                           .Select(e=>new 
+                               {   e.FirstName, 
+                                   e.LastName,
+                                   e.MiddleName,
+                                   e.JobTitle,
+                                   e.Salary
+                               }).ToList();
+            return string.Join(Environment.NewLine, employees.Select(e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}"));
         }
     }
 }
