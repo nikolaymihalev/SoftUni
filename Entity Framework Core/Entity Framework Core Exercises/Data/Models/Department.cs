977b3d3e7d015d7ac705ac45e6a304d22c93589a
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-10-27 06:26:49 +0000

    Setting up project

---
+++ Entity Framework Core/Entity Framework Core Exercises/Data/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entity_Framework_Core_Exercises.Data.Models
+{
+    public partial class Department
+    {
+        public Department()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        public int DepartmentId { get; set; }
+        public string Name { get; set; } = null!;
+        public int ManagerId { get; set; }
+
+        public virtual Employee Manager { get; set; } = null!;
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ Entity Framework Core/Entity Framework Core Exercises/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Entity_Framework_Core_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
