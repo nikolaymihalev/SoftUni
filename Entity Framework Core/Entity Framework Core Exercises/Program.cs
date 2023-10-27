3d22f5ca647eede4583ed4abe5d6a42d1030f6f1
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-10-27 08:15:34 +0000

    04. Employees with Salary Over 50 000

--- Entity Framework Core/Entity Framework Core Exercises/Program.cs
+++ Entity Framework Core/Entity Framework Core Exercises/Program.cs
@@ -8,10 +8,14 @@ namespace Entity_Framework_Core_Exercises
         {
             SoftUniContext context = new SoftUniContext();
             
-            //03. Employee Full Information
+            //03. Employee Full Information Test
             Console.WriteLine(GetEmployeesFullInformation(context));
+            
+            //04. Employees with Salary Over 50 000 Test
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
         }
-
+        
+        //03. Employee Full Information
         public static string GetEmployeesFullInformation(SoftUniContext context)
         {
             var employees = context.Employees
@@ -23,6 +27,19 @@ namespace Entity_Framework_Core_Exercises
                                    e.Salary
                                }).ToList();
             return string.Join(Environment.NewLine, employees.Select(e => $"{e.FirstName} {e.LastName} {e.MiddleName} {e.JobTitle} {e.Salary:f2}"));
+        }
+        //04. Employees with Salary Over 50 000
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e=>new 
+                {   e.FirstName, 
+                    e.Salary
+                })
+                .Where(e=>e.Salary>50000)
+                .OrderBy(e=>e.FirstName)
+                .ToList();
+            return string.Join(Environment.NewLine, employees.Select(e => $"{e.FirstName} - {e.Salary:f2}"));
         }
     }
 }
