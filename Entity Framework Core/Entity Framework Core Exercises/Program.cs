86975f32747974b64895503ea96652b0ef0cee9a
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-10-27 08:45:51 +0000

    05. Employees from Research and Development

--- Entity Framework Core/Entity Framework Core Exercises/Program.cs
+++ Entity Framework Core/Entity Framework Core Exercises/Program.cs
@@ -13,6 +13,9 @@ namespace Entity_Framework_Core_Exercises
             
             //04. Employees with Salary Over 50 000 Test
             Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+            
+            //05. Employees from Research and Development Test
+            Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
         }
         
         //03. Employee Full Information
@@ -40,6 +43,24 @@ namespace Entity_Framework_Core_Exercises
                 .OrderBy(e=>e.FirstName)
                 .ToList();
             return string.Join(Environment.NewLine, employees.Select(e => $"{e.FirstName} - {e.Salary:f2}"));
+        }
+        //05. Employees from Research and Development
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .Select(e =>
+                    new
+                    {
+                        e.FirstName,
+                        e.LastName,
+                        e.Department.Name,
+                        e.Salary
+                    })
+                .OrderBy(e => e.Salary)
+                .ThenBy(e => e.FirstName);
+                
+            return string.Join(Environment.NewLine,employees.Select(x=>$"{x.FirstName} {x.LastName} from {x.Name} - {x.Salary:c2}"));
         }
     }
 }
