ac6744fbf70f44083268d5fb2f54a362c3d16f04
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-10-31 08:56:00 +0000

    Arrangement

---
+++ Entity Framework Core/Entity Relations Exercises/Football Betting/P02_FootballBetting.Data.Models/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+        public string LogoUrl { get; set; }
+        [MaxLength(3)]
+        public string Initials { get; set; }
+        public decimal Budget { get; set; }
+
+        public int PrimaryKitColorId { get; set; }
+        [ForeignKey(nameof(PrimaryKitColorId))]
+        public virtual Color PrimaryKitColor { get; set; }
+
+        public int SecondaryKitColorId { get; set; }
+        [ForeignKey(nameof(SecondaryKitColorId))]
+        public virtual Color SecondaryKitColor { get; set; }
+    }
+}
