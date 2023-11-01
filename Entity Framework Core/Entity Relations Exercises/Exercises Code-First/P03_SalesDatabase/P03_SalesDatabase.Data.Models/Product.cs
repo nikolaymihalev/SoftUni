abefae5f3a7fa160335705f4d267dfc5ed786b58
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-01 12:22:41 +0000

    Start Sales Exercise

---
+++ Entity Framework Core/Entity Relations Exercises/Exercises Code-First/P03_SalesDatabase/P03_SalesDatabase.Data.Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_SalesDatabase.P03_SalesDatabase.Data.Models
+{
+    internal class Product
+    {
+    }
+}
---
+++ Entity Framework Core/Entity Relations Exercises/Exercises Code-First/P03_SalesDatabase/P03_SalesDatabase.Data/SalesContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_SalesDatabase.P03_SalesDatabase.Data
+{
+    public class SalesContext:DbContext
+    {
+        const string ConnectionString = "Server=.;Database=Sales;Trusted_Connection=True;MultipleActiveResultSets=true;Encrypt=false";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
