7e993a365da9212cf462cd6e0e21838e52a7a8f5
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-01 12:16:45 +0000

    Updating

--- Entity Framework Core/Entity Relations Exercises/Exercises Code-First/P01_HospitalDatabase/StartUp.cs
+++ Entity Framework Core/Entity Relations Exercises/Exercises Code-First/P01_HospitalDatabase/StartUp.cs
@@ -1,9 +1,12 @@
-﻿namespace P01_HospitalDatabase
+﻿using P01_HospitalDatabase.Data;
+
+namespace P01_HospitalDatabase
 {
     public class StartUp
     {
         static void Main(string[] args)
         {
+            HospitalContext context = new HospitalContext();
             Console.WriteLine("Hello, World!");
         }
     }
