51be76f63a48ee2f9bcaeaae674c336dd1bb16bb
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-03 08:47:28 +0000

    Output for album info method

--- Entity Framework Core/LINQ Exercises/MusicHub/StartUp.cs
+++ Entity Framework Core/LINQ Exercises/MusicHub/StartUp.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace MusicHub
 {
@@ -38,7 +39,29 @@ namespace MusicHub
                     TotalAlbumPrice=a.Price
                 }).OrderByDescending(x=>x.TotalAlbumPrice);
             
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var album in albumInfo)
+            {
+                sb.AppendLine($"-AlbumName: {album.AlbumName}")
+                    .AppendLine($"-ReleaseDate: {album.ReleaseDate}")
+                    .AppendLine($"-ProducerName: {album.ProducerName}")
+                    .AppendLine($"-Songs:");
+                if (album.AlbumSongs.Any())
+                {
+                    int count = 1;
+                    foreach (var song in album.AlbumSongs)
+                    {
+                        sb.AppendLine($"---#{count++}")
+                            .AppendLine($"---SongName: {song.SongName}")
+                            .AppendLine($"---Price: {song.SongPrice}")
+                            .AppendLine($"---Writer: {song.SongWriterName}");
+                    }
+                }
+
+                sb.AppendLine($"AlbumPrice: {album.TotalAlbumPrice:f2}");
+            }
+            return sb.ToString().Trim();
         }
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
