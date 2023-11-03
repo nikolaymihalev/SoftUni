8305bb9f1a5040ece334bd3b2cf9fb0ec33f3e77
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-03 07:25:09 +0000

    Create Song class

---
+++ Entity Framework Core/LINQ Exercises/MusicHub/Data/Models/Song.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MusicHub.Data.Models.Enums;
+
+namespace MusicHub.Data.Models;
+
+public class Song
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(20)]
+    public string Name { get; set; }
+
+    [Required]
+    public TimeSpan Duration { get; set; }
+
+    [Required]
+    public DateTime CreatedOn { get; set; }
+
+    public Genre Genre { get; set; }
+
+    public decimal Price { get; set; }
+    public int AlbumId { get; set; }
+    [ForeignKey(nameof(AlbumId))]
+    public Album Album { get; set; }
+    public int WriterId { get; set; }
+    [ForeignKey(nameof(WriterId))]
+    public Writer Writer { get; set; }
+
+    public ICollection<SongPerformer> SongPerformers { get; set; }
+}
