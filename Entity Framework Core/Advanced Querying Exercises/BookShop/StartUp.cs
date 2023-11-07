089eb66dfdd6e5c823614522337bb7956b793f55
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-07 08:02:35 +0000

    06. Get Books By Category

--- Entity Framework Core/Advanced Querying Exercises/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying Exercises/BookShop/StartUp.cs
@@ -62,12 +62,32 @@
         public static string GetBooksNotReleasedIn(BookShopContext context, int year)
         {
             var books = context.Books
-                .Where(b => b.ReleaseDate.HasValue && b.ReleaseDate.Value.Year != year)
                 .Select(b => new 
                 {
                     b.Title,
                     b.ReleaseDate
                 })
+                .Where(b => b.ReleaseDate.HasValue && b.ReleaseDate.Value.Year != year)
+                .ToList();
+
+            return String.Join(Environment.NewLine, books.Select(b => b.Title));
+        }
+
+        //06
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[] categories= input.Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                                        .Select(c=>c.ToLower())
+                                        .ToArray();
+
+            var books = context.Books
+                .Where(b => b.BookCategories.Any(bc=> categories.Contains(bc.Category.Name.ToLower())))
+                .Select(b => new
+                {
+                    b.Title,       
+                    b.BookCategories
+                })
+                .OrderBy(b => b.Title)
                 .ToList();
 
             return String.Join(Environment.NewLine, books.Select(b => b.Title));
