b35b313b6650bff4ceecaba7ac67007857276bac
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-07 07:33:09 +0000

    02. Books By Age Restriction

--- Entity Framework Core/Advanced Querying Exercises/BookShop/StartUp.cs
+++ Entity Framework Core/Advanced Querying Exercises/BookShop/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿namespace BookShop
 {
+    using BookShop.Models.Enums;
     using Data;
     using Initializer;
 
@@ -8,7 +9,28 @@
         public static void Main()
         {
             using var db = new BookShopContext();
-            DbInitializer.ResetDatabase(db);
+            //DbInitializer.ResetDatabase(db);
+
+            string input=Console.ReadLine();
+            Console.WriteLine(GetBooksByAgeRestriction(db,input));
+        }
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command) 
+        {
+            if (!Enum.TryParse<AgeRestriction>(command, true, out var ageRestriction)) 
+            {
+                return $"{command} is not a valid age restriction";
+            }
+            var books = context.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => new 
+                { 
+                    b.Title 
+                })
+                .OrderBy(b=>b.Title)
+                .ToList();
+
+
+            return String.Join(Environment.NewLine, books.Select(b=>b.Title));
         }
     }
 }
