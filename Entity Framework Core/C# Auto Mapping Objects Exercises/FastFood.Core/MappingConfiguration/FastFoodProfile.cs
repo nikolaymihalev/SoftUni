df53449685b041d0844c4d5c9f2eae73eb8c8679
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-10 07:51:11 +0000

    Create categories

---
+++ Entity Framework Core/C# Auto Mapping Objects Exercises/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,26 @@
+﻿namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Core.ViewModels.Categories;
+    using FastFood.Models;
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            //Categories
+            CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x=>x.Name, y=> y.MapFrom(z=>z.CategoryName));
+
+            CreateMap<Category, CategoryAllViewModel>();
+        }
+    }
+}
