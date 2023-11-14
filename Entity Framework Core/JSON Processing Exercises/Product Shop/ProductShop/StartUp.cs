f4064f0d4f1388b91322758dddad014474d39f62
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-14 08:53:12 +0000

    01. Import users

---
+++ Entity Framework Core/JSON Processing Exercises/Product Shop/ProductShop/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+        }
+
+        //01. Import users
+        public static string ImportUsers(ProductShopContext context, string inputJson) 
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+            return $"Successfullty imported {users.Length}";
+        }
+    }
+}
