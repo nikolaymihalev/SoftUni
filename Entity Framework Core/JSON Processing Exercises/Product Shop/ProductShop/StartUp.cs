6918f64e2666a462588b3a12bf4722b9b684f50c
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-14 09:17:47 +0000

    Import products, categories and category products
    2, 3, 4 exercises

--- Entity Framework Core/JSON Processing Exercises/Product Shop/ProductShop/StartUp.cs
+++ Entity Framework Core/JSON Processing Exercises/Product Shop/ProductShop/StartUp.cs
@@ -19,5 +19,43 @@ namespace ProductShop
             context.SaveChanges();
             return $"Successfullty imported {users.Length}";
         }
+        
+        //02. Import products
+        public static string ImportProducts(ProductShopContext context, string inputJson) 
+        {
+            var products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+            if (products != null) 
+            {
+                context.Products.AddRange(products);
+                context.SaveChanges();
+            }
+            return $"Successfullty imported {products.Length}";
+        }
+        
+        //03. Import categories
+        public static string ImportCategories(ProductShopContext context, string inputJson) 
+        {
+            var categories = JsonConvert.DeserializeObject<Category[]>(inputJson);
+            var validCategories = categories.Where(c => c.Name is not null).ToArray();
+
+            if (validCategories != null) 
+            {
+                context.Categories.AddRange(validCategories);
+                context.SaveChanges();
+            }
+            return $"Successfullty imported {validCategories.Length}";
+        }
+        
+        //04. Import category products
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson) 
+        {
+            var categoryProducts = JsonConvert.DeserializeObject<CategoryProduct[]>(inputJson);
+            if (categoryProducts != null) 
+            {
+                context.CategoriesProducts.AddRange(categoryProducts);
+                context.SaveChanges();
+            }
+            return $"Successfullty imported {categoryProducts.Length}";
+        }
     }
 }
