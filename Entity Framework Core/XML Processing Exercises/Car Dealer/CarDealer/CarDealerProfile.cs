6a5dca609fe27a9963becbce2ed35d7673b5a96a
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-17 07:51:28 +0000

    09. Import Suppliers

---
+++ Entity Framework Core/XML Processing Exercises/Car Dealer/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<ImportSupplierDTO, Supplier>();
+        }
+    }
+}
---
+++ Entity Framework Core/XML Processing Exercises/Car Dealer/CarDealer/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+        }
+
+        //09. Import Suppliers
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ImportSupplierDTO[]), new XmlRootAttribute("Suppliers"));
+
+            using var reader = new StringReader(inputXml);
+
+            ImportSupplierDTO[] importSupplierDTOs = (ImportSupplierDTO[])xmlSerializer.Deserialize(reader);
+
+            var mapper = GetMapper();
+            Supplier[] suppliers = mapper.Map<Supplier[]>(importSupplierDTOs);
+
+            context.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
+        }
+
+        static Mapper GetMapper() 
+        {
+            var cfg = new MapperConfiguration(c => c.AddProfile<CarDealerProfile>());
+            return new Mapper(cfg);
+        }
+    }
+}
