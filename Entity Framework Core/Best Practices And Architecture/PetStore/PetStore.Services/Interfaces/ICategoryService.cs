58f24a745200d7c43f14daa9763477bc17892e82
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-21 08:43:07 +0000

    Update cotroller

--- Entity Framework Core/Best Practices And Architecture/PetStore/PetStore.Services/Interfaces/ICategoryService.cs
+++ Entity Framework Core/Best Practices And Architecture/PetStore/PetStore.Services/Interfaces/ICategoryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PetStore.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace PetStore.Services.Interfaces
 {
     public interface ICategoryService
     {
+        Task<IEnumerable<Category>> GetAll();
     }
 }
--- Entity Framework Core/Best Practices And Architecture/PetStore/PetStore.Web/Controllers/CategoriesController.cs
+++ Entity Framework Core/Best Practices And Architecture/PetStore/PetStore.Web/Controllers/CategoriesController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PetStore.Services.Interfaces;
 
 namespace PetStore.Web.Controllers
 {
     public class CategoriesController : Controller
     {
-        public IActionResult Index()
+        readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
         {
-            return View();
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var service = await _categoryService.GetAll();
+            return View(service);
         }
     }
 }
