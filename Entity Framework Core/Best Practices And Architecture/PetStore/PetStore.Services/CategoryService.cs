0c75cd47f47ef18556add99560f8bbdebe9de904
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-21 08:30:55 +0000

    Create Category Service class

---
+++ Entity Framework Core/Best Practices And Architecture/PetStore/PetStore.Services/CategoryService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetStore.Data;
+using PetStore.Data.Models;
+using PetStore.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore.Services
+{
+    public class CategoryService:ICategoryService
+    {
+        readonly ApplicationDbContext _context;
+
+        public CategoryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAll() 
+        {
+            return await _context.Categories.ToListAsync();
+        }
+    }
+}
