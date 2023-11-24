5b3472aa54e8bee00e8c1b595be2260d814367de
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-24 17:57:11 +0000

    Preparing project

---
+++ Entity Framework Core/Blog - Workshop/App/Blog/Blog.Data/BlogDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data
+{
+    public class BlogDBContext : IdentityDbContext<IdentityUser>
+    {
+        public BlogDBContext()
+        {
+            
+        }
+        public BlogDBContext(DbContextOptions<BlogDBContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
---
+++ Entity Framework Core/Blog - Workshop/App/Blog/Blog/Program.cs
@@ -0,0 +1,46 @@
+using Blog.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace Blog
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<BlogDBContext>(options=>
+                        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+                        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<BlogDBContext>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
