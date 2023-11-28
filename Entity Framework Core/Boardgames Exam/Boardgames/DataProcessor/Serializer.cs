583c74b8dda4ce20d3ad2f77e20e2683d4bf3fdb
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-28 08:09:23 +0000

    03. Export Creators

--- Entity Framework Core/Boardgames Exam/Boardgames/DataProcessor/Serializer.cs
+++ Entity Framework Core/Boardgames Exam/Boardgames/DataProcessor/Serializer.cs
@@ -1,12 +1,42 @@
 ﻿namespace Boardgames.DataProcessor
 {
     using Boardgames.Data;
+    using Boardgames.DataProcessor.ExportDto;
+    using System.Text;
+    using System.Xml.Serialization;
 
     public class Serializer
     {
         public static string ExportCreatorsWithTheirBoardgames(BoardgamesContext context)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ExportCreatorDTO[]), new XmlRootAttribute("Creators"));
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+            using StringWriter sw = new StringWriter(sb);
+
+            ExportCreatorDTO[] creators = context.Creators
+                .Where(c => c.Boardgames.Any())
+                .Select(c => new ExportCreatorDTO
+                {
+                    CreatorName = c.FirstName + " " + c.LastName,
+                    BoardgamesCount = c.Boardgames.Count(),
+                    Boardgames = c.Boardgames.Select(b => new ExportCreatorBoardgamesDTO
+                    {
+                        BoardgameName = b.Name,
+                        BoardgameYearPublished = b.YearPublished
+                    })
+                    .OrderBy(b => b.BoardgameName)
+                    .ToArray()
+                })
+                .OrderByDescending(c => c.BoardgamesCount)
+                .ThenBy(c => c.CreatorName)
+                .ToArray();
+
+            xmlSerializer.Serialize(sw, creators, namespaces);
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportSellersWithMostBoardgames(BoardgamesContext context, int year, double rating)
