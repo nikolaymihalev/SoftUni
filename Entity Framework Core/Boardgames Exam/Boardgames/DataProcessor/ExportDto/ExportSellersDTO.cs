f7d4f1303a8f9dd13010a992b45ddfecc182483e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-28 08:07:02 +0000

    Create export dtos

---
+++ Entity Framework Core/Boardgames Exam/Boardgames/DataProcessor/ExportDto/ExportSellersDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boardgames.DataProcessor.ExportDto
+{
+    public class ExportSellersDTO
+    {
+        public string Name { get; set; }
+        public string Website { get; set; }
+        public ExportSellerBoardgamesDTO[] Boardgames { get; set; }
+    }
+}
