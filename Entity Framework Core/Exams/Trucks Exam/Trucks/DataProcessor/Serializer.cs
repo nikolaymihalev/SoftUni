1cd4a5373af80a98797b61ce6d3bf65647cb76da
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-29 10:27:53 +0000

    03. Export Despatchers

--- Entity Framework Core/Exams/Trucks Exam/Trucks/DataProcessor/Serializer.cs
+++ Entity Framework Core/Exams/Trucks Exam/Trucks/DataProcessor/Serializer.cs
@@ -1,12 +1,40 @@
 ﻿namespace Trucks.DataProcessor
 {
+    using AutoMapper.QueryableExtensions;
+    using AutoMapper;
     using Data;
+    using System.Text;
+    using System.Xml.Serialization;
+    using Trucks.DataProcessor.ExportDto;
 
     public class Serializer
     {
         public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
         {
-            throw new NotImplementedException();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<TrucksProfile>();
+            });
+
+            StringBuilder sb = new StringBuilder();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ExportDespatcherDto[]), new XmlRootAttribute("Despatchers"));
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter sw = new StringWriter(sb);
+
+            ExportDespatcherDto[] despatcherDtos =
+                context
+                .Despatchers
+                .Where(d => d.Trucks.Any())
+                .ProjectTo<ExportDespatcherDto>(config)
+                .OrderByDescending(d => d.TrucksCount)
+                .ThenBy(d => d.Name)
+                .ToArray();
+            xmlSerializer.Serialize(sw, despatcherDtos, namespaces);
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
