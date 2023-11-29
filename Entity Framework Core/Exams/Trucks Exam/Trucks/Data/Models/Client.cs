3ca834c95973d5891cd1c596c4ec78ff24d3c860
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-29 09:35:09 +0000

    Create db models

---
+++ Entity Framework Core/Exams/Trucks Exam/Trucks/Data/Models/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trucks.Data.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string Nationality { get; set; }
+        
+        [Required]
+        public string Type { get; set; }
+
+        public ICollection<ClientTruck> ClientsTrucks { get; set; } = new List<ClientTruck>();
+    }
+}
---
+++ Entity Framework Core/Exams/Trucks Exam/Trucks/Data/Models/Truck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trucks.Data.Models.Enums;
+
+namespace Trucks.Data.Models
+{
+    public class Truck
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string RegistrationNumber { get; set; }
+
+        [Required]
+        public string VinNumber { get; set; }
+
+        public int TankCapacity { get; set; }
+        public int CargoCapacity { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+        
+        [Required]
+        public MakeType MakeType { get; set; }
+
+        [Required]
+        public int DespatcherId { get; set; }
+
+        [ForeignKey(nameof(DespatcherId))]
+        public Despatcher Despatcher { get; set; }
+
+        public ICollection<ClientTruck> ClientsTrucks { get; set; } = new List<ClientTruck>();
+    }
+}
