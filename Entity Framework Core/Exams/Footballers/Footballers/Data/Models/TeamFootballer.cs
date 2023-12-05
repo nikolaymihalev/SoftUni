b81c9478a7e7420229cb7fbf71a567877b8ecbda
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-05 07:49:12 +0000

    Create db models

---
+++ Entity Framework Core/Exams/Footballers/Footballers/Data/Models/TeamFootballer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footballers.Data.Models
+{
+    public class TeamFootballer
+    {
+        [Required]
+        public int TeamId { get; set; }
+
+        [ForeignKey(nameof(TeamId))]
+        public Team Team { get; set; }
+
+        [Required]
+        public int FootballerId { get; set; }
+
+        [ForeignKey(nameof(FootballerId))]
+        public Footballer Footballer { get; set; }
+    }
+}
