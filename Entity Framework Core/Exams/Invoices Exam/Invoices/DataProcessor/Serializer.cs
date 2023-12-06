a490827d39bbd36d2aebdd31b95f1456422751ed
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-06 09:40:54 +0000

    05. Export clients

--- Entity Framework Core/Exams/Invoices Exam/Invoices/DataProcessor/Serializer.cs
+++ Entity Framework Core/Exams/Invoices Exam/Invoices/DataProcessor/Serializer.cs
@@ -1,14 +1,53 @@
 ﻿namespace Invoices.DataProcessor
 {
     using Invoices.Data;
+    using Invoices.DataProcessor.ExportDto;
     using Microsoft.EntityFrameworkCore;
     using Newtonsoft.Json;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml.Serialization;
 
     public class Serializer
     {
         public static string ExportClientsWithTheirInvoices(InvoicesContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ExportClientDto[]), new XmlRootAttribute("Clients"));
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add(string.Empty, string.Empty);
+
+            using StringWriter stringWriter = new StringWriter(sb);
+
+            var clients = context.Clients.AsNoTracking()
+                .Where(c=>c.Invoices.Any(i=>i.IssueDate>=date))
+                .Select(c=>new ExportClientDto
+                {
+                    ClientName = c.Name,
+                    VatNumber = c.NumberVat,
+                    InvoicesCount = c.Invoices.Count(),
+                    Invoices = c.Invoices
+                            .Where(i => i.IssueDate >= date)
+                            .OrderBy(i=>i.IssueDate)
+                            .ThenByDescending(i=>i.DueDate)
+                            .ToArray()
+                            .Select(i=>new ExportInvoicesDto 
+                            {
+                                InvoiceNumber = i.Number,
+                                InvoiceAmount = i.Amount,
+                                DueDate = i.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                                Currency = i.CurrencyType.ToString()
+                            })
+                            .ToArray()
+                })
+                .OrderByDescending(c=>c.InvoicesCount)
+                .ThenBy(c=>c.ClientName)
+                .ToArray();
+
+            xmlSerializer.Serialize(stringWriter, clients, ns);
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportProductsWithMostClients(InvoicesContext context, int nameLength)
@@ -22,8 +61,8 @@
                     Category = p.CategoryType.ToString(),
                     Clients = p.ProductsClients
                                 .Where(pc => pc.Client.Name.Length >= nameLength)
-                                .OrderBy(c=>c.Client.Name)
                                 .ToArray()
+                                .OrderBy(c=>c.Client.Name)
                                 .Select(c=>new 
                                 {
                                     Name = c.Client.Name,
