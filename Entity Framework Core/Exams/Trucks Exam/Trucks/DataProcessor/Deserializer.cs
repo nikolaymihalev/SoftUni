5ec34795fbf3328296795d3bae0597f2605de438
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-07 08:08:34 +0000

    02. Import Client

--- Entity Framework Core/Exams/Trucks Exam/Trucks/DataProcessor/Deserializer.cs
+++ Entity Framework Core/Exams/Trucks Exam/Trucks/DataProcessor/Deserializer.cs
@@ -4,6 +4,7 @@
     using System.Text;
     using System.Xml.Serialization;
     using Data;
+    using Newtonsoft.Json;
     using Trucks.Data.Models;
     using Trucks.Data.Models.Enums;
     using Trucks.DataProcessor.ImportDto;
@@ -78,7 +79,53 @@
         }
         public static string ImportClient(TrucksContext context, string jsonString)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            ImportClientDto[] importClientDtos = JsonConvert.DeserializeObject<ImportClientDto[]>(jsonString);
+            List<Client> clients = new List<Client>();
+
+            foreach (var dto in importClientDtos) 
+            {
+                if (!IsValid(dto)) 
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                if (dto.Type == "usual") 
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Client client = new Client 
+                {
+                    Name = dto.Name,
+                    Nationality = dto.Nationality,
+                    Type = dto.Type
+                };
+
+                foreach (var id in dto.Trucks.Distinct())
+                {
+                    Truck truck = context.Trucks.Find(id);
+
+                    if (truck == null)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    client.ClientsTrucks.Add(new ClientTruck { Client = client ,Truck = truck });
+                }
+
+                clients.Add(client);
+                sb.AppendLine(string.Format(SuccessfullyImportedClient,client.Name,client.ClientsTrucks.Count));
+            }
+
+            context.Clients.AddRange(clients);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         private static bool IsValid(object dto)
