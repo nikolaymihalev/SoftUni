eef6582724fd28b393bfe88a1e77327afe8cbf0f
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-07 08:00:58 +0000

    01. Import Despatcher

--- Entity Framework Core/Exams/Trucks Exam/Trucks/DataProcessor/Deserializer.cs
+++ Entity Framework Core/Exams/Trucks Exam/Trucks/DataProcessor/Deserializer.cs
@@ -1,8 +1,12 @@
 ﻿namespace Trucks.DataProcessor
 {
     using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using System.Xml.Serialization;
     using Data;
-
+    using Trucks.Data.Models;
+    using Trucks.Data.Models.Enums;
+    using Trucks.DataProcessor.ImportDto;
 
     public class Deserializer
     {
@@ -16,7 +20,61 @@
 
         public static string ImportDespatcher(TrucksContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportDespatcherDto[]), new XmlRootAttribute("Despatchers"));
+            using StringReader reader = new StringReader(xmlString);
+
+            ImportDespatcherDto[] importDespatcherDtos = xmlSerializer.Deserialize(reader) as ImportDespatcherDto[];
+            List<Despatcher> despatchers = new List<Despatcher>();
+
+            foreach (var dto in importDespatcherDtos) 
+            {
+                if (!IsValid(dto)) 
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                if (string.IsNullOrEmpty(dto.Position)) 
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Despatcher despatcher = new Despatcher 
+                {
+                    Name = dto.Name,
+                    Position = dto.Position
+                };
+
+                foreach (var truckDto in dto.Trucks) 
+                {
+                    if (!IsValid(truckDto)) 
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    despatcher.Trucks.Add(new Truck 
+                    {
+                        RegistrationNumber = truckDto.RegistrationNumber,
+                        VinNumber = truckDto.VinNumber,
+                        TankCapacity = truckDto.TankCapacity,
+                        CargoCapacity = truckDto.CargoCapacity,
+                        CategoryType = (CategoryType)truckDto.CategoryType,
+                        MakeType = (MakeType)truckDto.MakeType,
+                    });
+                }
+
+                despatchers.Add(despatcher);
+                sb.AppendLine(string.Format(SuccessfullyImportedDespatcher, despatcher.Name, despatcher.Trucks.Count));
+            }
+
+            context.Despatchers.AddRange(despatchers);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
         public static string ImportClient(TrucksContext context, string jsonString)
         {
---
+++ Entity Framework Core/Exams/Trucks Exam/Trucks/DataProcessor/ImportDto/ImportDespatcherDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ImportDto
+{
+    [XmlType("Despatcher")]
+    public class ImportDespatcherDto
+    {
+        [Required]
+        [MaxLength(40)]
+        [MinLength(2)]
+        public string Name { get; set; }
+        public string Position { get; set; }
+
+        [XmlArray("Trucks")]
+        public ImportTruckDto[] Trucks { get; set; }
+    }
+}
