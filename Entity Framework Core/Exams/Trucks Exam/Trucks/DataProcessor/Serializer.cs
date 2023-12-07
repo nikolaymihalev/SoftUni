2df7f76faa559c85cc6398dab4b66aa2ae7003eb
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-07 08:21:44 +0000

    03. Export Clients

--- Entity Framework Core/Exams/Trucks Exam/Trucks/DataProcessor/Serializer.cs
+++ Entity Framework Core/Exams/Trucks Exam/Trucks/DataProcessor/Serializer.cs
@@ -1,6 +1,8 @@
 ﻿namespace Trucks.DataProcessor
 {
     using Data;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
 
     public class Serializer
     {
@@ -11,7 +13,33 @@
 
         public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
         {
-            throw new NotImplementedException();
+            var clients = context.Clients.AsNoTracking()
+                .Where(c=>c.ClientsTrucks.Any(ct=>ct.Truck.TankCapacity>=capacity))
+                .Select(c=>new 
+                {
+                    Name = c.Name,
+                    Trucks = c.ClientsTrucks
+                            .Where(ct => ct.Truck.TankCapacity >= capacity)
+                            .OrderBy(t=>t.Truck.MakeType)
+                            .ThenByDescending(ct=>ct.Truck.CargoCapacity)
+                            .ToArray()
+                            .Select(ct=>new 
+                            {
+                                TruckRegistrationNumber = ct.Truck.RegistrationNumber,
+                                VinNumber = ct.Truck.VinNumber,
+                                TankCapacity = ct.Truck.TankCapacity,
+                                CargoCapacity = ct.Truck.CargoCapacity,
+                                CategoryType = ct.Truck.CategoryType.ToString(),
+                                MakeType = ct.Truck.MakeType.ToString(),
+                            })
+                            .ToArray()
+                })
+                .OrderByDescending(c=>c.Trucks.Length)
+                .ThenBy(c=>c.Name)
+                .Take(10)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(clients,Formatting.Indented);
         }
     }
 }
