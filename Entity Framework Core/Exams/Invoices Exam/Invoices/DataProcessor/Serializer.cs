ee0d3576172f66c895a96f34ef2ab6f8ab3e16a5
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-08 16:52:55 +0000

    04. Export Clients

--- Entity Framework Core/Exams/Invoices Exam/Invoices/DataProcessor/Serializer.cs
+++ Entity Framework Core/Exams/Invoices Exam/Invoices/DataProcessor/Serializer.cs
@@ -6,7 +6,29 @@
     {
         public static string ExportClientsWithTheirInvoices(InvoicesContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var config = new MapperConfiguration(cfg => 
+            {
+                cfg.AddProfile<InvoicesProfile>();
+            });
+
+            StringBuilder sb = new StringBuilder();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ClientExportDto[]), new XmlRootAttribute("Clients"));
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter sw = new StringWriter(sb);
+
+            ClientExportDto[] clientsDtos = context
+                .Clients
+                .Where(c => c.Invoices.Any(ci => ci.IssueDate >= date))
+                .ProjectTo<ClientExportDto>(config)
+                .OrderByDescending(c => c.InvoicesCount)
+                .ThenBy(c => c.Name)
+                .ToArray();
+            xmlSerializer.Serialize(sw, clientsDtos, namespaces);
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportProductsWithMostClients(InvoicesContext context, int nameLength)
