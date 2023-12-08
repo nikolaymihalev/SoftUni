d10aaf842c425e73c02db36e990789d9eb28f643
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-08 16:51:15 +0000

    01. Import Clients

--- Entity Framework Core/Exams/Invoices Exam/Invoices/DataProcessor/Deserializer.cs
+++ Entity Framework Core/Exams/Invoices Exam/Invoices/DataProcessor/Deserializer.cs
@@ -19,9 +19,55 @@
 
         public static string ImportClients(InvoicesContext context, string xmlString)
         {
-            throw new NotImplementedException();
-        }
+            StringBuilder sb = new StringBuilder();
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ClientImportDto[]), new XmlRootAttribute("Clients"));
 
+            using StringReader stringReader = new StringReader(xmlString);
+
+            ClientImportDto[] clientsDto = (ClientImportDto[])xmlSerializer.Deserialize(stringReader);
+
+            List<Client> clients = new List<Client>();
+
+            foreach (ClientImportDto clientDto in clientsDto)
+            {
+                if (!IsValid(clientDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Client c = new Client()
+                {
+                    Name = clientDto.Name,
+                    NumberVat = clientDto.NumberVat
+                };
+
+                foreach (AddressImportDto addressDto in clientDto.Addresses)
+                {
+                    if (!IsValid(addressDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Address a = new Address()
+                    {
+                        StreetName = addressDto.StreetName,
+                        StreetNumber = addressDto.StreetNumber,
+                        PostCode = addressDto.PostCode,
+                        City = addressDto.City,
+                        Country = addressDto.Country
+                    };
+
+                    c.Addresses.Add(a);
+                }
+                clients.Add(c);
+                sb.AppendLine(String.Format(SuccessfullyImportedClients, c.Name));
+            }
+            context.Clients.AddRange(clients);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
 
         public static string ImportInvoices(InvoicesContext context, string jsonString)
         {
