2cd93256487e995570e1e36609c73885f9e79529
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-09 12:27:20 +0000

    03. Export coaches

--- Entity Framework Core/Exams/Footballers Exam/Footballers/DataProcessor/Serializer.cs
+++ Entity Framework Core/Exams/Footballers Exam/Footballers/DataProcessor/Serializer.cs
@@ -6,7 +6,30 @@
     {
         public static string ExportCoachesWithTheirFootballers(FootballersContext context)
         {
-            throw new NotImplementedException();
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<FootballersProfile>();
+            });
+            var mapper = new Mapper(config);
+
+            StringBuilder sb = new StringBuilder();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ExportCoachDto[]), new XmlRootAttribute("Coaches"));
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter sw = new StringWriter(sb);
+
+            ExportCoachDto[] coachDtos = context
+                .Coaches
+                .Where(c => c.Footballers.Any())
+                .ProjectTo<ExportCoachDto>(config)
+                .OrderByDescending(c => c.FootballersCount)
+                .ThenBy(c => c.Name)
+                .ToArray();
+            xmlSerializer.Serialize(sw, coachDtos, namespaces);
+            return sb.ToString().TrimEnd();
         }
 
         public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
