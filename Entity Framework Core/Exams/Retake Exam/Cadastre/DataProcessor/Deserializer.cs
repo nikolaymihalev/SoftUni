4ec24b32bd82227eef13454e06e2978d9f7e88bd
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-11 17:37:44 +0000

    01. Import district

--- Entity Framework Core/Exams/Retake Exam/Cadastre/DataProcessor/Deserializer.cs
+++ Entity Framework Core/Exams/Retake Exam/Cadastre/DataProcessor/Deserializer.cs
@@ -14,7 +14,102 @@
 
         public static string ImportDistricts(CadastreContext dbContext, string xmlDocument)
         {
-            throw new NotImplementedException();
+             StringBuilder sb =new StringBuilder();
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportDistrictDto[]), new XmlRootAttribute("Districts"));
+            using StringReader stringReader = new StringReader(xmlDocument);
+
+            ImportDistrictDto[] importDistrictDtos = xmlSerializer.Deserialize(stringReader) as ImportDistrictDto[];
+            List<District> districts = new List<District>();
+
+            foreach (var dto in importDistrictDtos) 
+            {
+                if (!IsValid(dto)) 
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                District distInDB = dbContext.Districts.FirstOrDefault(x => x.Name == dto.Name);
+                if (distInDB != null) 
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                District district = new District() 
+                {
+                    Name = dto.Name,
+                    PostalCode = dto.PostalCode,
+                    Region = dto.Region
+                };
+
+                foreach(var property in dto.Properties) 
+                {
+                    if (!IsValid(property)) 
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    DateTime dateOfAcquisition;
+                    bool isDateValid = DateTime.TryParseExact(
+                        property.DateOfAcquisition,
+                        "dd/MM/yyyy",
+                        CultureInfo.InvariantCulture,
+                        DateTimeStyles.None,
+                        out dateOfAcquisition);
+                    if (!isDateValid)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    //----------PropertyIdentifier
+                    if (district.Properties.Any(x => x.PropertyIdentifier == property.PropertyIdentifier))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Property propDB = dbContext.Properties.FirstOrDefault(x => x.PropertyIdentifier == property.PropertyIdentifier);
+                    if (propDB != null) 
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    //----------Address
+                    if (district.Properties.Any(x => x.Address == property.Address))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Property propDB_A = dbContext.Properties.FirstOrDefault(x => x.Address == property.Address);
+                    if (propDB_A != null)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    district.Properties.Add(new Property 
+                    {
+                        PropertyIdentifier = property.PropertyIdentifier,
+                        Area = property.Area,
+                        Details = property.Details,
+                        Address = property.Address,
+                        DateOfAcquisition = dateOfAcquisition,
+                        District = district,
+                    });
+                }
+                sb.AppendLine(string.Format(SuccessfullyImportedDistrict,district.Name, district.Properties.Count));
+                districts.Add(district);
+            }
+
+            dbContext.Districts.AddRange(districts);
+            dbContext.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportCitizens(CadastreContext dbContext, string jsonDocument)
