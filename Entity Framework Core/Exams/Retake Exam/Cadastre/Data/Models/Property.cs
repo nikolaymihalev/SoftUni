ad414d4d964e91127bb13f407b5e258af42b0c79
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-11 17:34:28 +0000

    Create db models

---
+++ Entity Framework Core/Exams/Retake Exam/Cadastre/Data/Models/Property.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastre.Data.Models
+{
+    public class Property
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string PropertyIdentifier { get; set; }
+
+        [Required]
+        public int Area { get; set; }
+
+        [StringLength(500)]
+        public string Details { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Required]
+        public DateTime DateOfAcquisition { get; set; }
+
+        [Required]
+        public int DistrictId { get; set; }
+
+        [ForeignKey(nameof(DistrictId))]
+        public District District { get; set; }
+
+        public ICollection<PropertyCitizen> PropertiesCitizens { get; set; } = new List<PropertyCitizen>();
+    }
+}
---
+++ Entity Framework Core/Exams/Retake Exam/Cadastre/Data/Models/PropertyCitizen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastre.Data.Models
+{
+    public class PropertyCitizen
+    {
+        [Required]
+        public int PropertyId { get; set; }
+
+        [ForeignKey(nameof(PropertyId))]
+        public Property Property { get; set; }
+
+        [Required]
+        public int CitizenId { get; set; }
+
+        [ForeignKey(nameof(CitizenId))]
+        public Citizen Citizen { get; set; }
+    }
+}
