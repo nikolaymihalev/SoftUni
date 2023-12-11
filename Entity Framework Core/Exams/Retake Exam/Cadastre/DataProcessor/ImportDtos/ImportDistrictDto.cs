500d1c04fa5dcb3b263eb3af47884bd96435e373
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-11 17:35:36 +0000

    Create import dto models

---
+++ Entity Framework Core/Exams/Retake Exam/Cadastre/DataProcessor/ImportDtos/ImportDistrictDto.cs
@@ -0,0 +1,31 @@
+﻿using Cadastre.Data.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Cadastre.DataProcessor.ImportDtos
+{
+    [XmlType("District")]
+    public class ImportDistrictDto
+    {
+        [Required]
+        [MaxLength(80)]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]{2}-\d{5}$")]
+        public string PostalCode { get; set; }
+        
+        [XmlAttribute("Region")]
+        [EnumDataType(typeof(Region))]
+        public Region Region { get; set; }
+
+        [XmlArray("Properties")]
+        public ImportPropertyDto[] Properties { get; set; }
+    }
+}
---
+++ Entity Framework Core/Exams/Retake Exam/Cadastre/DataProcessor/ImportDtos/ImportPropertyDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Cadastre.DataProcessor.ImportDtos
+{
+    [XmlType("Property")]
+    public class ImportPropertyDto
+    {
+        [Required]
+        [MaxLength(20)]
+        [MinLength(16)]
+        public string PropertyIdentifier { get; set; }
+
+        [Required]
+        [Range(0, 2147483647)]
+        public int Area { get; set; }
+
+        [MaxLength(500)]
+        [MinLength(5)]
+        public string Details { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        [MinLength(5)]
+        public string Address { get; set; }
+
+        [Required]
+        public string DateOfAcquisition { get; set; }
+    }
+}
