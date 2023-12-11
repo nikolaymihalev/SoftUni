22e69c5a1db9b10353c47bbd46905fa971341b66
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-11 17:38:42 +0000

    Create ExportPropertyDto.cs

---
+++ Entity Framework Core/Exams/Retake Exam/Cadastre/DataProcessor/ExportDtos/ExportPropertyDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Cadastre.DataProcessor.ExportDtos
+{
+    [XmlType("Property")]
+    public class ExportPropertyDto
+    {
+        public string PropertyIdentifier { get; set; }
+        public string Area { get; set; }
+        public string DateOfAcquisition { get; set; }
+
+        [XmlAttribute("postal-code")]
+        public string PostalCode { get; set; }
+    }
+}
