110a2dd1de467b68ec384301466974e82b5cb780
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-12-11 17:39:21 +0000

    03. Export properties

--- Entity Framework Core/Exams/Retake Exam/Cadastre/DataProcessor/Serializer.cs
+++ Entity Framework Core/Exams/Retake Exam/Cadastre/DataProcessor/Serializer.cs
@@ -6,7 +6,36 @@ namespace Cadastre.DataProcessor
     {
         public static string ExportPropertiesWithOwners(CadastreContext dbContext)
         {
-           throw new NotImplementedException();
+           DateTime date;
+            bool isDateValid = DateTime.TryParseExact(
+                "01/01/2000",
+                "dd/MM/yyyy",
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None,
+                out date);
+
+            var properties = dbContext.Properties.AsNoTracking()
+                .Where(x=>x.DateOfAcquisition>=date)
+                .OrderByDescending(x => x.DateOfAcquisition)
+                .ThenBy(x => x.PropertyIdentifier)
+                .Select(x=> new 
+                {
+                    PropertyIdentifier = x.PropertyIdentifier,
+                    Area = x.Area,
+                    Address = x.Address,
+                    DateOfAcquisition = x.DateOfAcquisition.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    Owners = x.PropertiesCitizens
+                                .OrderBy(x=>x.Citizen.LastName)
+                                .ToArray()
+                                .Select(x=> new 
+                                {
+                                    LastName = x.Citizen.LastName,
+                                    MaritalStatus = x.Citizen.MaritalStatus.ToString(),
+                                }).ToArray()
+                })                
+                .ToArray();
+
+            return JsonConvert.SerializeObject(properties,Formatting.Indented);
         }
 
         public static string ExportFilteredPropertiesWithDistrict(CadastreContext dbContext)
