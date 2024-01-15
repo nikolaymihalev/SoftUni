feab0e5cc7d2564edd66de557b703cc5a64fcf98
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-15 08:30:37 +0000

    Create new controller

---
+++ ASP. NET/HTTP Protocol/HttpBasic/Controllers/StoreListController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpBasic.Controllers
+{
+    public class StoreListController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddItem() 
+        {
+            return View();
+        }
+    }
+}
