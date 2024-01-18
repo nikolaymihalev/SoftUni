cd6e2aa4723b91363753ac1f5a32371bf64c86fd
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-18 10:44:36 +0000

    Create form for editing data for custom class

--- ASP. NET/HTTP Protocol/ASP.NET Core Introduction/AspNetCoreIntro/Controllers/IntroController.cs
+++ ASP. NET/HTTP Protocol/ASP.NET Core Introduction/AspNetCoreIntro/Controllers/IntroController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreIntro.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreIntro.Controllers
 {
@@ -13,6 +14,20 @@ namespace AspNetCoreIntro.Controllers
         {
             ViewBag.Title = "GetNumber";
             return View("Number",number);
+        }
+
+        public IActionResult GetStudentData() 
+        {
+            ViewBag.Title = "GetStudentData";
+
+            var student = new Student
+            {
+                Id =1,
+                Name = "Jhon",
+                Email= "jhon@gmail.com"
+            };
+
+            return View(student);
         }
     }
 }
---
+++ ASP. NET/HTTP Protocol/ASP.NET Core Introduction/AspNetCoreIntro/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreIntro.Models
+{
+    public  class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
