4a49b7ab4f1e7cf1225824277898e3bb2217a865
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-21 11:18:22 +0000

    03. 50 Numbers

---
+++ ASP. NET/ASP.NET Core Exercise/MVCInroDemo/MVCInroDemo/Controllers/NumbersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCInroDemo.Controllers
+{
+    public class NumbersController : Controller
+    {
+        private readonly ILogger<NumbersController> logger;
+
+        public NumbersController(ILogger<NumbersController> _logger)
+        {
+            logger=_logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
