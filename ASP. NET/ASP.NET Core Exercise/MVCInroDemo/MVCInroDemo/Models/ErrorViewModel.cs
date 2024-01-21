1f6e3df889c62ddaaeb64faff427d34430886b2b
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-21 10:48:33 +0000

    01. ViewBag and ViewData

---
+++ ASP. NET/ASP.NET Core Exercise/MVCInroDemo/MVCInroDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCInroDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
