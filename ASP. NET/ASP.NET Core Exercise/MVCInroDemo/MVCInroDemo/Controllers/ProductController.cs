77c03f6c681dee04ae5933c087cc3784595fd2d1
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-21 12:02:13 +0000

    Create table with custom class and list

---
+++ ASP. NET/ASP.NET Core Exercise/MVCInroDemo/MVCInroDemo/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCInroDemo.Models;
+
+namespace MVCInroDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IEnumerable<ProductViewModel> products = new List<ProductViewModel>() 
+        {
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 2.4
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 4.7
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
---
+++ ASP. NET/ASP.NET Core Exercise/MVCInroDemo/MVCInroDemo/Models/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCInroDemo.Models
+{
+    /// <summary>
+    /// Product model
+    /// </summary>
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Price { get; set; }
+    }
+}
