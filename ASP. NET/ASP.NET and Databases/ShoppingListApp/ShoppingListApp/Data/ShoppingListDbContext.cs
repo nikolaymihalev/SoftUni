107add771a7c997f1e5e73b776fc9155298e777e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-26 08:26:50 +0000

    Create and perform migration

--- ASP. NET/ASP.NET and Databases/ShoppingListApp/ShoppingListApp/Data/ShoppingListDbContext.cs
+++ ASP. NET/ASP.NET and Databases/ShoppingListApp/ShoppingListApp/Data/ShoppingListDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShoppingListApp.Data.Models;
 
 namespace ShoppingListApp.Data
 {
@@ -8,5 +9,7 @@ namespace ShoppingListApp.Data
             : base(options)
         {            
         }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductNote> ProductNotes { get; set; }
     }
 }
