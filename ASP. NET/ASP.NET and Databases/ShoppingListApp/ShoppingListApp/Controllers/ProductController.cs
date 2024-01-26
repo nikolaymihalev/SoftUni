787b5cdb948e8bb0a527247784c0fd34875efe5f
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-26 09:49:20 +0000

    Create add operation

--- ASP. NET/ASP.NET and Databases/ShoppingListApp/ShoppingListApp/Controllers/ProductController.cs
+++ ASP. NET/ASP.NET and Databases/ShoppingListApp/ShoppingListApp/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShoppingListApp.Contracts;
 using ShoppingListApp.Data;
+using ShoppingListApp.Models;
 
 namespace ShoppingListApp.Controllers
 {
@@ -13,10 +14,30 @@ namespace ShoppingListApp.Controllers
             productService = _productService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             var model = await productService.GetAllAsync();
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Add() 
+        {
+            var model = new ProductViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductViewModel model) 
+        {
+            if (ModelState.IsValid == false) 
+            {
+                return View(model);
+            }
+            await productService.AddProductAsync(model);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
