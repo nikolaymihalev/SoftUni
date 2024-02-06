f3877815551e589ee28b468553c0c4d4a26b4805
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-06 19:24:15 +0000

    Create authorize controller

--- ASP. NET/ASP.NET Identity/MVCIdentityDemo/MVCIdentityDemo/Controllers/HomeController.cs
+++ ASP. NET/ASP.NET Identity/MVCIdentityDemo/MVCIdentityDemo/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 
 namespace MVCIdentityDemo.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
