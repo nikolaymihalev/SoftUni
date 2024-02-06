f23cb304abcaa6722a5da0e099b4dd64df50c157
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-06 19:27:53 +0000

    Add allow anonymous attribute

--- ASP. NET/ASP.NET Identity/MVCIdentityDemo/MVCIdentityDemo/Controllers/HomeController.cs
+++ ASP. NET/ASP.NET Identity/MVCIdentityDemo/MVCIdentityDemo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MVCIdentityDemo.Models;
 using System.Diagnostics;
 
@@ -13,6 +14,7 @@ namespace MVCIdentityDemo.Controllers
             _logger = logger;
         }
 
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
