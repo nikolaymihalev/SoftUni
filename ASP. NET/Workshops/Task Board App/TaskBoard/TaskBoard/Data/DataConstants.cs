d8ae6040b5a900a18a1185dc3be4dda7ae681899
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-09 18:41:57 +0000

    Create board entity

--- ASP. NET/Workshops/Task Board App/TaskBoard/TaskBoard/Data/DataConstants.cs
+++ ASP. NET/Workshops/Task Board App/TaskBoard/TaskBoard/Data/DataConstants.cs
@@ -8,7 +8,12 @@
             public const int TitleMinLength = 5; 
             public const int DescriptionMaxLength = 1000; 
             public const int DescriptionMinLength = 10; 
+        }
 
+        public static class Board
+        {
+            public const int NameMaxLength = 30;
+            public const int NameMinLength = 3;
         }
     }
 }
--- ASP. NET/Workshops/Task Board App/TaskBoard/TaskBoard/Data/Models/Board.cs
+++ ASP. NET/Workshops/Task Board App/TaskBoard/TaskBoard/Data/Models/Board.cs
@@ -1,6 +1,16 @@
-﻿namespace TaskBoard.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskBoard.Data.Models
 {
     public class Board
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.Board.NameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        public IEnumerable<Task> Tasks { get; set; } = new List<Task>();
     }
 }
