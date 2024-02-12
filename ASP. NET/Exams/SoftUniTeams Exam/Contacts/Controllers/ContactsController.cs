bbfc8e7d21f187364de6fea3ceae9726c905b0b1
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-12 15:49:10 +0000

    05. Added Team page

--- ASP. NET/Exams/SoftUniTeams Exam/Contacts/Controllers/ContactsController.cs
+++ ASP. NET/Exams/SoftUniTeams Exam/Contacts/Controllers/ContactsController.cs
@@ -1,12 +1,14 @@
 ﻿using Contacts.Data;
 using Contacts.Data.Models;
 using Contacts.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 
 namespace Contacts.Controllers
 {
+    [Authorize]
     public class ContactsController : Controller
     {
         private readonly ContactsDbContext context;
@@ -21,7 +23,7 @@ namespace Contacts.Controllers
         {
             var model = await context.Contacts
                 .AsNoTracking()
-                .Select(x=> new ContactInfoViewModel(
+                .Select(x => new ContactInfoViewModel(
                     x.Id,
                     x.FirstName,
                     x.LastName,
@@ -34,21 +36,21 @@ namespace Contacts.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddToTeam(int contactId) 
+        public async Task<IActionResult> AddToTeam(int contactId)
         {
             var contact = await context.Contacts
                 .Where(x => x.Id == contactId)
                 .Include(x => x.ApplicationUsersContacts)
                 .FirstOrDefaultAsync();
 
-            if (contact == null) 
+            if (contact == null)
             {
                 return BadRequest();
             }
 
             string userId = GetUserId();
 
-            if (!contact.ApplicationUsersContacts.Any(x => x.ApplicationUserId == userId)) 
+            if (!contact.ApplicationUsersContacts.Any(x => x.ApplicationUserId == userId))
             {
                 contact.ApplicationUsersContacts.Add(new ApplicationUserContact()
                 {
@@ -63,7 +65,7 @@ namespace Contacts.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult>RemoveFromTeam(int contactId) 
+        public async Task<IActionResult> RemoveFromTeam(int contactId)
         {
             var contact = await context.Contacts
                 .Where(x => x.Id == contactId)
@@ -79,7 +81,7 @@ namespace Contacts.Controllers
 
             var auc = contact.ApplicationUsersContacts.FirstOrDefault(x => x.ApplicationUserId == userId);
 
-            if (auc is null) 
+            if (auc is null)
             {
                 return BadRequest();
             }
@@ -89,6 +91,27 @@ namespace Contacts.Controllers
             await context.SaveChangesAsync();
 
             return RedirectToAction(nameof(Team));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Team() 
+        {
+            string userId = GetUserId();
+
+            var model = await context.ApplicationUsersContacts
+                .AsNoTracking()
+                .Where(x=>x.ApplicationUserId==userId)
+                .Select(x=> new ContactInfoViewModel(
+                    x.Contact.Id,
+                    x.Contact.FirstName,
+                    x.Contact.LastName,
+                    x.Contact.Email,
+                    x.Contact.PhoneNumber,
+                    x.Contact.Address,
+                    x.Contact.Website))
+                .ToListAsync();
+
+            return View(model);
         }
 
         private string GetUserId() 
