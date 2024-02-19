d593739b1f97bc0a3d8b5d61562c24573460071c
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-19 16:04:16 +0000

    04. Solved Add seminar view and operation

--- ASP. NET/Exams/SeminarHub Exam/SeminarHub/Controllers/SeminarController.cs
+++ ASP. NET/Exams/SeminarHub Exam/SeminarHub/Controllers/SeminarController.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SeminarHub.Data;
 using SeminarHub.Data.Constants;
+using SeminarHub.Data.Models;
 using SeminarHub.Models;
+using System.Globalization;
+using System.Security.Claims;
 
 namespace SeminarHub.Controllers
 {
@@ -32,6 +35,60 @@ namespace SeminarHub.Controllers
                 .ToListAsync();
 
             return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = new SeminarFormViewModel();
+            model.Categories = await GetCategories();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(SeminarFormViewModel model)
+        {
+            DateTime date = DateTime.Now;
+
+            if (!DateTime.TryParseExact(model.DateAndTime,
+                ValidationConstants.DateFormat,
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None, out date))
+            {
+                ModelState.AddModelError(nameof(model.DateAndTime), $"Invalid date! Format must be: {ValidationConstants.DateFormat}");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                model.Categories = await GetCategories();
+                return View(model);
+            }
+
+            var seminar = new Seminar()
+            {
+                Topic = model.Topic,
+                Lecturer = model.Lecturer,
+                Details = model.Details,
+                OrganizerId = GetUserId(),
+                DateAndTime = date,
+                Duration = model.Duration,
+                CategoryId = model.CategoryId
+            };
+
+            await context.Seminars.AddAsync(seminar);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(All));
+        }
+
+        private async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await context.Categories.AsNoTracking().ToListAsync();
+        }
+
+        private string GetUserId()
+        {
+            return User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
         }
     }
 }
---
+++ ASP. NET/Exams/SeminarHub Exam/SeminarHub/Models/SeminarFormViewModel.cs
@@ -0,0 +1,64 @@
+﻿using SeminarHub.Data.Constants;
+using SeminarHub.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace SeminarHub.Models
+{
+    /// <summary>
+    /// View model for adding seminar 
+    /// </summary>
+    public class SeminarFormViewModel
+    {
+        /// <summary>
+        /// Seminar's topic 
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.SeminarTopicMaxLength,
+            MinimumLength = ValidationConstants.SeminarTopicMinLength,
+            ErrorMessage = ErrorMessagesConstants.StringLengthErrorMessage)]
+        public string Topic { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Seminar's lecturer 
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.SeminarLecturerMaxLength,
+            MinimumLength = ValidationConstants.SeminarLecturerMinLength,
+            ErrorMessage = ErrorMessagesConstants.StringLengthErrorMessage)]
+        public string Lecturer { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Seminar's details
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
+        [StringLength(ValidationConstants.SeminarDetailsMaxLength,
+            MinimumLength = ValidationConstants.SeminarDetailsMinLength,
+            ErrorMessage = ErrorMessagesConstants.StringLengthErrorMessage)]
+        public string Details { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Seminar's date and time
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
+        public string DateAndTime { get; set; } = string.Empty;
+
+
+        /// <summary>
+        /// Seminar's duration
+        /// </summary>
+        [Range(ValidationConstants.SeminarDurationMinValue,ValidationConstants.SeminarDurationMaxValue)]
+        public int Duration { get; set; }
+
+
+        /// <summary>
+        /// Seminar's category identifier
+        /// </summary>
+        [Required(ErrorMessage = ErrorMessagesConstants.RequireErrorMessage)]
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// Collection for all kinds of category
+        /// </summary>
+        public IEnumerable<Category> Categories { get; set; } = new List<Category>();
+    }
+}
