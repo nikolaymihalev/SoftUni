fc2cabdc782de2925b5cfeb40e5a4c8d85795a20
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-22 13:55:45 +0000

    Create custom model binder

---
+++ ASP. NET/Routing and Binding, Views, DI and Services/AspNetCoreAdvancedDemo/AspNetCoreAdvancedDemo/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace AspNetCoreAdvancedDemo.ModelBinders
+{
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext.ValueProvider
+                .GetValue(bindingContext.ModelName);
+
+            if(valueResult!= ValueProviderResult.None && !string.IsNullOrEmpty(valueResult.FirstValue)) 
+            {
+                decimal result = 0m;
+                bool success = false;
+
+                try
+                {
+                    string strValue = valueResult.FirstValue.Trim();
+                    strValue = strValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    strValue = strValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    result = Convert.ToDecimal(strValue);
+                    success = true;
+                }
+                catch (FormatException fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
