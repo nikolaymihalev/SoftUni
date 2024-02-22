2e657e621ef0730ed3e6c028b6ddf50a858182c6
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-02-22 13:59:46 +0000

    Create custom attribute

---
+++ ASP. NET/Routing and Binding, Views, DI and Services/AspNetCoreAdvancedDemo/AspNetCoreAdvancedDemo/Attributes/IsBefore.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace AspNetCoreAdvancedDemo.Attributes
+{
+    public class IsBefore : ValidationAttribute
+    {
+        private const string DateTimeFormat = "dd/MM/yyyy";
+        private readonly DateTime date;
+        public IsBefore(string dateInput)
+        {
+            date = DateTime.ParseExact(dateInput, DateTimeFormat, CultureInfo.InvariantCulture);
+        }
+        protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+        {
+            if ((DateTime)value >= date) 
+                return new ValidationResult(ErrorMessage);
+
+            return ValidationResult.Success;
+        }
+    }
+}
