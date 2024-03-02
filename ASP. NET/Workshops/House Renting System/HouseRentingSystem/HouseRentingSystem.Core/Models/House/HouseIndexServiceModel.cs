85c221d4e97ae5d9547135e8229882d2b5618ce3
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-02 16:25:43 +0000

    Created House Index Service model

--- ASP. NET/Workshops/House Renting System/HouseRentingSystem/HouseRentingSystem.Core/Models/House/HouseIndexServiceModel.cs
+++ ASP. NET/Workshops/House Renting System/HouseRentingSystem/HouseRentingSystem.Core/Models/House/HouseIndexServiceModel.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HouseRentingSystem.Core.Models.House
+﻿namespace HouseRentingSystem.Core.Models.House
 {
     public class HouseIndexServiceModel
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string ImageUrl { get; set; } = string.Empty;
     }
 }
