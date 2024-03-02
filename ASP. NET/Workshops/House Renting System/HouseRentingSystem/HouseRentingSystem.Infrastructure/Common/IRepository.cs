8f440feda6ef0d0dd7c4afb372927c8479ac8c0b
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-02 16:40:31 +0000

    Added get all methods to repository

--- ASP. NET/Workshops/House Renting System/HouseRentingSystem/HouseRentingSystem.Infrastructure/Common/IRepository.cs
+++ ASP. NET/Workshops/House Renting System/HouseRentingSystem/HouseRentingSystem.Infrastructure/Common/IRepository.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HouseRentingSystem.Infrastructure.Common
+﻿namespace HouseRentingSystem.Infrastructure.Common
 {
-    internal interface IRepository
+    public interface IRepository
     {
+        IQueryable<T> All<T>() where T : class; 
+        IQueryable<T> AllReadOnly<T>() where T : class;
     }
 }
--- ASP. NET/Workshops/House Renting System/HouseRentingSystem/HouseRentingSystem.Infrastructure/Common/Repository.cs
+++ ASP. NET/Workshops/House Renting System/HouseRentingSystem/HouseRentingSystem.Infrastructure/Common/Repository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HouseRentingSystem.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,26 @@ namespace HouseRentingSystem.Infrastructure.Common
 {
     public class Repository : IRepository
     {
+        private readonly DbContext context;
+
+        public Repository(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        private DbSet<T> DbSet<T>() where T:class
+        {
+            return context.Set<T>();
+        }
+
+        public IQueryable<T> All<T>() where T: class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>().AsNoTracking();
+        }
     }
 }
