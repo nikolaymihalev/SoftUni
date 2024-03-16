3e6fce1a632314fb51d1e4f266b650ff596c0df5
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-03-16 17:34:33 +0000

    Extend application role entity

---
+++ ASP. NET/ASP.NET Identity, External Login and JSON Web Tokens/IdentityAdvancedDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using IdentityAdvancedDemo.Data.IdentityModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAdvancedDemo.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ASP. NET/ASP.NET Identity, External Login and JSON Web Tokens/IdentityAdvancedDemo/Data/IdentityModels/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityAdvancedDemo.Data.IdentityModels
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+        [MaxLength(50)]
+        public string? BGName { get; set; }
+    }
+}
