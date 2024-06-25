8c2bb28b6aaa1fff5f7cc8fdeaaf513697c1a44d
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-06-25 11:24:37 +0000

    Completed 05. Math Operations

---
+++ JS Front-End/JS Syntax Fundamentals - Lab/05. Math Operations/exercise.js
@@ -0,0 +1,26 @@
+function solve(num1, num2, operation){
+    if (operation === "+") {
+
+        console.log(num1+num2);
+    }else if (operation === "-") {
+
+        console.log(num1-num2);
+
+    }else if (operation === "*") {
+
+        console.log(num1*num2);
+
+    }else if (operation === "/") {
+
+        console.log(num1/num2);
+
+    }else if (operation === "%") {
+
+        console.log(num1%num2);
+
+    }else if (operation === "**") {
+
+        console.log(num1**num2);
+
+    } 
+}
