ff75c6734bf8c1512bbcc63cf65b44251268f71d
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-15 07:28:14 +0000

    Completed 01. Format Grade

--- JS Front-End/Functions and Statements - Lab/01. Format Grade/exercise.js
+++ JS Front-End/Functions and Statements - Lab/01. Format Grade/exercise.js
@@ -1,3 +1,17 @@
-function solve() {
+function solve(grade) {
+    let result = "";
     
+    if(grade < 3){
+        result = 'Fail';
+    }else if (grade < 3.5){
+        result = 'Poor';
+    }else if (grade < 4.5){
+        result = 'Good';
+    }else if (grade < 5.5){
+        result = 'Very good';
+    }else if (grade <= 6){
+        result = 'Excellent';
+    }
+
+    console.log(`${result} (${grade < 3 ? 2 : grade.toFixed(2)})`);
 }
