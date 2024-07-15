8da87cba9bdbab5a2595ce452980cadf588110ee
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-15 08:36:38 +0000

    Completed 05. Simple Calculator

--- JS Front-End/Functions and Statements - Lab/05. Simple Calculator/exercise.js
+++ JS Front-End/Functions and Statements - Lab/05. Simple Calculator/exercise.js
@@ -1,3 +1,20 @@
-function solve() {
-    
+function solve(x1, x2, operation) {
+    let result = 0;
+
+    switch (operation) {
+        case 'multiply':
+            result = x1 * x2;
+            break;
+        case 'divide':
+            result = x1 / x2;
+            break;
+        case 'add':
+            result = x1 + x2;
+            break;
+        case 'subtract':
+            result = x1 - x2;
+            break;
+    }
+
+    console.log(result);
 }
