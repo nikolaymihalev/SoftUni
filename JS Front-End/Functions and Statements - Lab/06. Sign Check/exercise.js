64725bc1b8b23f75bfd11bada82c0e29684581d3
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-15 08:40:51 +0000

    Completed 06. Sign Check

--- JS Front-End/Functions and Statements - Lab/06. Sign Check/exercise.js
+++ JS Front-End/Functions and Statements - Lab/06. Sign Check/exercise.js
@@ -1,3 +1,16 @@
-function solve() {
-    
+function solve(x1, x2, x3) {
+    let array = [x1, x2, x3];
+    let count = 0;
+
+    for (let i = 0; i < 3; i++) {
+        if(array[i] < 0){
+            count++;
+        }
+    }
+
+    if(count === 3 || count === 1){
+        console.log('Negative');
+    }else{
+        console.log('Positive');
+    }    
 }
