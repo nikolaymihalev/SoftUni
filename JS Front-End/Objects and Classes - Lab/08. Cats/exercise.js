4c8bc0216b199f569c9904cb7b98c04ddbdd5853
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-18 08:38:06 +0000

    Completed 08. Cats

---
+++ JS Front-End/Objects and Classes - Lab/08. Cats/exercise.js
@@ -0,0 +1,18 @@
+function solve(input) {
+    class  Cat{
+        constructor(name, age){
+            this.name = name;
+            this.age = age;
+        }
+
+        meow(){
+            console.log(`${this.name}, age ${this.age} says Meow`);
+        }
+    }
+
+
+    input.map(entry => entry.split(' '))
+        .map(([name, age]) => new Cat(name,age))
+        .forEach(cat => cat.meow()        
+    );
+}
