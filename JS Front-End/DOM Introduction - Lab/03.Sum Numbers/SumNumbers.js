c13bf8423519c51b43aae403b28f646059f9a2c7
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-26 09:11:59 +0000

    Completed 03. Sum Numbers

--- JS Front-End/DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
+++ JS Front-End/DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,10 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    const num1 = document.getElementById('num1');
+    const num2 = document.getElementById('num2');
+    const sum = document.getElementById('sum');
+
+    const firstNum = Number(num1.value);
+    const secondNum = Number(num2.value);
+
+    sum.value = firstNum+secondNum;
 }
