57de7ee8a64b03ff12fd332e6c9a3134f29aab16
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-29 07:44:15 +0000

    Completed 03. Accordion

--- JS Front-End/DOM Introduction - Exercise/03. Accordion/accordion.js
+++ JS Front-End/DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    const buttonEl = document.querySelector(".head span.button");
+    const extraEl = document.querySelector("#extra");
+    const isHidden = extraEl.style.display === "none";
+
+    if (isHidden || !extraEl.style.display) {
+        extraEl.style.display = "block";
+        buttonEl.textContent = "Less";
+    } else {
+        extraEl.style.display = "none";
+        buttonEl.textContent = "More";
+    }
 }
