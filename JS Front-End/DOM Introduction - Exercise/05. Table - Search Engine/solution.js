9f48b9467910f06ec24f7c41862bd7f63c09f752
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-29 08:03:51 +0000

    Completed 05. Table – Search Engine

--- JS Front-End/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ JS Front-End/DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,26 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const tableRowEls = document.querySelectorAll("table.container tbody tr");
 
+      function getMatchElements(input) {
+         return [...tableRowEls].filter((rowEl) => rowEl.textContent.toLocaleLowerCase().includes(input.toLowerCase()));
+      }
+
+      function clearPrevios() {
+         [...tableRowEls].forEach((rowEl) => {
+            rowEl.classList.remove("select");
+         })
+      }
+
+      clearPrevios();
+
+      const searchValue = document.getElementById("searchField").value;
+
+      const matchRows = getMatchElements(searchValue);
+
+      matchRows.forEach((matchRow) => {
+         matchRow.classList.add("select");
+      });
    }
 }
