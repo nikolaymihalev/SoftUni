95e4caf0b3b0318d05dad0bc15d6efb02968941e
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-07-29 08:28:04 +0000

    Completed 06. Format the Text

--- JS Front-End/DOM Introduction - Exercise/06. Format the Text/solution.js
+++ JS Front-End/DOM Introduction - Exercise/06. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  const inputEl = document.getElementById("input");
+  const outputEl = document.getElementById("output");
+
+  function createParagraphEl(text) {
+    const pEl = document.createElement("p");
+    pEl.textContent = text;
+    return pEl;
+  }
+
+  const sentences = inputEl.value.split(".").filter(Boolean).map((x) => x.trim());
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    const currentText = sentences.slice(i, i + 3).join(". ").concat(".");
+    const currentParag = createParagraphEl(currentText);
+    outputEl.appendChild(currentParag);
+  }
 }
