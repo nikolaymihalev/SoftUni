33f472b94889484eeb7ddd3b61da8c70b8f15790
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-05 18:24:28 +0000

    Completed 03. Add / Delete

--- JS Front-End/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
+++ JS Front-End/DOM Manipulation and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    const itemsElement = document.getElementById('items');
+    const newItemTextElement = document.getElementById('newItemText');
+
+    const liElement = document.createElement('li');
+    liElement.textContent = newItemTextElement.value;
+
+    const deleteButton = document.createElement('a');
+    deleteButton.textContent = '[Delete]';
+    deleteButton.href = '#';
+
+    deleteButton.addEventListener('click', (e) => {
+        e.currentTarget.parentElement.remove();
+    });
+
+    liElement.append(deleteButton);
+
+    itemsElement.appendChild(liElement);
+
+    newItemTextElement.value = '';
 }
