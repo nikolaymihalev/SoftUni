14a7bb8961456e3fe79be9d06958baa0d59eeded
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-05 17:02:01 +0000

    Completed 02. Delete from Table

--- JS Front-End/DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
+++ JS Front-End/DOM Manipulation and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const customerTableElement = document.getElementById('customers');
+    const inputElement = document.querySelector('input[type=text][name=email]');
+    const resultElement = document.getElementById('result');
+
+    const searchEmail = inputElement.value;
+
+    const tdElements = customerTableElement.querySelectorAll('tbody td:last-child');
+
+    const searchElement = Array.from(tdElements).find(el => el.textContent === searchEmail);
+
+    if (searchElement) {
+        searchElement.parentElement.remove();
+        resultElement.textContent = 'Deleted.';
+    } else {
+        resultElement.textContent = 'Not found.';
+    }
 }
