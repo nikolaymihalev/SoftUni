96d61884189e62715e5e44eded04c689dcdf9389
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-05 19:11:04 +0000

    Completed 07. Shopping Cart

--- JS Front-End/DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ JS Front-End/DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-   //TODO...
+   const resultElement = document.querySelector('textarea[disabled]');
+   const checkoutButton = document.querySelector('button.checkout');
+   const productCatalogElement = document.querySelector('.shopping-cart');
+
+   let products = [];
+
+   productCatalogElement.addEventListener('click', (e) => {
+      if (e.target.tagName !== 'BUTTON')
+         return;
+
+      if (e.target.textContent.trim() !== 'Add')
+         return;
+
+      const productElement = e.target.closest('.product');
+      const name = productElement.querySelector('.product-title').textContent;
+      const price = Number(productElement.querySelector('.product-line-price').textContent);
+
+      resultElement.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+
+      products.push({
+         name,
+         price
+      });
+   });
+
+   checkoutButton.addEventListener('click', (e) => {
+      const totalPrice = products.reduce((price, product) => price + product.price, 0);
+      const productList = [...new Set(products.map(product => product.name))];
+
+      resultElement.value += `You bought ${productList.join(', ')} for ${totalPrice.toFixed(2)}.`;
+
+      document.querySelectorAll('button')
+         .forEach(el => el.setAttribute('disabled', 'disabled'));
+   })
 }
