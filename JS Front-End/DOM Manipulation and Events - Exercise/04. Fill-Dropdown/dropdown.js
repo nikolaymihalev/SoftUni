056d98c09d394deb4fbbc0d893b85608405852eb
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-06 08:50:04 +0000

    Completed 04. Fill Dropdown

--- JS Front-End/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Front-End/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    function createOptionElement(text, value) {
+        const option = document.createElement('option');
+        option.textContent = text;
+        option.value = value;
+
+        return option;
+    }
+
+    const [textInputEl, valueInputEl] = document.querySelectorAll('input');
+
+    document.getElementById('menu').appendChild(createOptionElement(textInputEl.value, valueInputEl.value));
+
+    textInputEl.value = '';
+    valueInputEl.value = '';
 }
