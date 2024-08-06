a473600765062d08c43f21098d99231ec70e25c6
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-06 09:02:37 +0000

    Completed 05. Encode and Decode Messages

--- JS Front-End/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS Front-End/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [encodeBtnEl, decodeBtnEl] = document.querySelectorAll("div button");
+    const [encodeTextareaEl, decodeTextareaEl] = document.querySelectorAll("div textarea");
+
+    function operateMessage(text, asciiDiff) {
+        return text
+            .split('')
+            .map((char) => {
+                const currentValue = char.charCodeAt(0);
+                return String.fromCharCode(currentValue + asciiDiff);
+            })
+            .join('');
+    }
+
+    function encodeMessageHandler() {
+        decodeTextareaEl.value = operateMessage(encodeTextareaEl.value, 1);
+        encodeTextareaEl.value = '';
+    }
+
+    function decodeMessageHandler() {
+        decodeTextareaEl.value = operateMessage(decodeTextareaEl.value, -1);
+    }
+
+    encodeBtnEl.addEventListener('click', encodeMessageHandler);
+    decodeBtnEl.addEventListener('click', decodeMessageHandler);
 }
