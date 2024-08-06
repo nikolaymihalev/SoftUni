63ce3618aaf9fbae1191c4eba7391d7bf58221f3
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-06 09:28:02 +0000

    Completed 09. JavaScript Quizz

--- JS Front-End/DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
+++ JS Front-End/DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,44 @@
 function solve() {
-  //TODO...
+  const sectionElements = document.querySelectorAll("section");
+
+  const correctAnswers = ["onclick", "JSON.stringify()", "A programming API for HTML and XML documents"];
+  const userAnswers = [];
+
+  function showUserResult(answers, rightAnswers) {
+    correctAnswersCount = answers.reduce((totalCount, currentAnswers, index) =>
+      currentAnswers === rightAnswers[index]
+        ? (totalCount += 1)
+        : totalCount,
+      0);
+
+    const headingResultEl = document.querySelector("ul#results h1");
+
+    headingResultEl.textContent = correctAnswersCount === rightAnswers.length
+      ? `You are recognized as top Javascript fan!`
+      : `You have ${correctAnswersCount} right answers`;
+
+    headingResultEl.parentElement.parentElement.style.display = "block";
+  }
+
+  function onClickHandler(event) {
+    const sectionToHide = sectionElements[userAnswers.length];
+    const sectionToShow = sectionElements[userAnswers.length + 1];
+
+    if (!!sectionToHide) {
+      sectionToHide.style.display = 'none';
+    }
+
+    if (!!sectionToShow) {
+      sectionToShow.style.display = 'block';
+    }
+
+
+    userAnswers.push(event.target.textContent.trim());
+
+    if (userAnswers.length === correctAnswers.length) {
+      showUserResult(userAnswers, correctAnswers);
+    }
+  }
+
+  document.querySelectorAll(".answer-wrap").forEach((wrapEl) => wrapEl.addEventListener('click', onClickHandler))
 }
