1cb99b7eb1ea801acb94d57d0fa1a4f56ec5ef35
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-06 07:41:18 +0000

    Completed 02. Time Converter

--- JS Front-End/DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
+++ JS Front-End/DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,56 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    const secondsToMinutes = (seconds) => seconds / 60;
+    const secondsToHours = (seconds) => secondsToMinutes(seconds) / 60;
+    const secondsToDays = (seconds) => secondsToHours(seconds) / 24;
+
+    const minutesToSeconds = (minutes) => minutes * 60;
+    const minutesToHours = (minutes) => minutes / 60;
+    const minutesToDays = (minutes) => minutesToHours(minutes) / 24;
+
+    const hoursToSeconds = (hours) => hours * 60 * 60;
+    const hoursToMinutes = (hours) => hours * 60;
+    const hoursToDays = (hours) => hours / 24;
+
+    const daysToSeconds = (days) => days * 24 * 120;
+    const daysToMinutes = (days) => days * 24 * 60;
+    const daysToHours = (days) => days * 24;
+
+    function onClickHandler(event) {
+        const rawId = event.target.getAttribute("id");
+        const inputId = rawId.replace("Btn", "");
+
+        const inputEl = document.querySelector(`input#${inputId}`);
+
+        switch (inputId) {
+            case 'seconds': {
+                document.getElementById('days').value = secondsToDays(inputEl.value);
+                document.getElementById('hours').value = secondsToHours(inputEl.value);
+                document.getElementById('minutes').value = secondsToMinutes(inputEl.value);
+            };
+                break;
+            case 'minutes': {
+                document.getElementById('days').value = minutesToDays(inputEl.value);
+                document.getElementById('hours').value = minutesToHours(inputEl.value);
+                document.getElementById('seconds').value = minutesToSeconds(inputEl.value);
+            };
+                break;
+            case 'hours': {
+                document.getElementById('days').value = hoursToDays(inputEl.value);
+                document.getElementById('minutes').value = hoursToMinutes(inputEl.value);
+                document.getElementById('seconds').value = hoursToSeconds(inputEl.value);
+            };
+                break;
+            case 'days': {
+                document.getElementById('hours').value = daysToHours(inputEl.value);
+                document.getElementById('minutes').value = daysToMinutes(inputEl.value);
+                document.getElementById('seconds').value = daysToSeconds(inputEl.value);
+            };
+                break;
+        }
+    }
+
+    document.querySelectorAll("input[type='button']").forEach((buttonEl) => {
+        buttonEl.addEventListener("click", onClickHandler);
+    })
 }
