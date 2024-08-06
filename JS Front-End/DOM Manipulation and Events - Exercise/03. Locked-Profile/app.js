8ca01fb92dbf931361f4a5c82157907ae2725931
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-08-06 08:40:07 +0000

    Completed 03. Locked Profile

--- JS Front-End/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS Front-End/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttonElements = document.querySelectorAll("div.profile button");
+
+    function showModeClickHandler(event) {
+        const button = event.target;
+
+        const [, unlockInputEl] = event.target.parentElement.querySelectorAll('input');
+
+        if (!unlockInputEl.checked)
+            return;
+
+        if (button.textContent === "Show more") {
+            button.parentElement.querySelector("div").style.display = "none";
+            button.textContent = "Hide it";
+        } else {
+            button.parentElement.querySelector('div').style.display = "block";
+            button.textContent = "Show more";
+        }
+    }
+
+    buttonElements.forEach((el) => {
+        el.addEventListener('click', showModeClickHandler)
+    });
 }
