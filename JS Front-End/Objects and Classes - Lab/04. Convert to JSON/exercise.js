4ea3edb49c3d3d69f9f3bf6205a53866ead9e095
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-09-19 17:21:43 +0300

    Completed 04. Convert to JSON

---
+++ JS Front-End/Objects and Classes - Lab/04. Convert to JSON/exercise.js
@@ -0,0 +1,9 @@
+function solve(firstName, lastName, color) {
+    let info = {
+        name: firstName,
+        lastName: lastName,
+        hairColor: color
+    };
+
+    console.log(JSON.stringify(info));
+}
